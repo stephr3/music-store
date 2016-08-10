9bd165a535bedfdec3a97bb0830f005e871c9360
Bhawani Parajuli and Stephanie Roth <bhawani@>
2016-08-10 09:45:14 -0700

    add filter to display albums by artists.

--- app/album-list.component.ts
+++ app/album-list.component.ts
@@ -2,12 +2,13 @@ import { Component } from 'angular2/core';
 import { AlbumComponent } from './album.component';
 import { Album } from './album.model';
 import { GenrePipe } from './genre.pipe';
+import { ArtistPipe } from './artist.pipe';
 
 @Component({
   selector: 'album-list',
   inputs: ['albumList'],
   directives: [AlbumComponent],
-  pipes: [GenrePipe],
+  pipes: [GenrePipe, ArtistPipe],
   template:
   `
   <select (change)="onChange($event.target.value)">
@@ -16,15 +17,24 @@ import { GenrePipe } from './genre.pipe';
     <option value="Rock">Rock</option>
     <option value="Folk">Folk</option>
   </select>
-  <album-display *ngFor = "#currentAlbum of albumList | genre:filterGenre" [album]="currentAlbum"></album-display>
+  <p>Search Albums by Artists</p>
+  <input value="all" #artistName>
+  <button (click)="showAlbum(artistName)">Search</button>
+  <album-display *ngFor = "#currentAlbum of albumList | genre:filterGenre | artist:filterArtist" [album]="currentAlbum"></album-display>
   `
 })
 
 export class AlbumListComponent {
   public albumList: Album[];
   public filterGenre: string = "all";
+  public filterArtist: string = "all";
 
   onChange(filterOption){
     this.filterGenre = filterOption;
+  }
+  showAlbum(filterOption: HTMLInputElement){
+    this.filterArtist = filterOption.value;
+    filterOption.value = "all";
+
   }
 }
---
+++ app/artist.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from 'angular2/core';
+import { Album } from './album.model';
+
+@Pipe({
+  name: "artist",
+  
+})
+export class ArtistPipe implements PipeTransform{
+  transform(input: Album[], args) {
+    var desiredArtist = args[0];
+    if(desiredArtist === "all"){
+      return input;
+    } else {
+      return input.filter(function(album){
+        return (album.artist === desiredArtist);
+      });
+    }
+  }
+}
